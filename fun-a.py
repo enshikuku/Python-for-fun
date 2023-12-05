d33054b8bd3b9d54411c4459cb48518e7e76ab7d
Shikuku39 <enshikuku39@gmail.com>
2023-12-05 13:46:08 +0300

    initial commit

---
+++ fun-a.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+speed(2)
+
+color('#900020')
+begin_fill()
+pensize(5)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(133)
+end_fill()
+
+hideturtle()
+done()
